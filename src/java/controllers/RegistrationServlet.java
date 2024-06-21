ca294fe505767aa44a894cbe5725c884e7863858
Hoang Long Duong <longheo242@gmail.com>
2024-06-21 12:34:19 +0700

    new update

--- src/java/controllers/RegistrationServlet.java
+++ src/java/controllers/RegistrationServlet.java
@@ -3,20 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller;
+package controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import table.accounts.AccountDAO;
+import table.accounts.AccountDTO;
 
 /**
  *
  * @author long
  */
-public class ViewHomePageServlet extends HttpServlet {
+public class RegistrationServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -26,13 +32,35 @@ public class ViewHomePageServlet extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
+        String userName = request.getParameter("txtUsername");
+        String passWord = request.getParameter("txtPassword");
+        String email = request.getParameter("txtEmail");
+        String fullName = request.getParameter("txtFullName");
+        String phone = request.getParameter("txtPhone");
+        String url = "registration.jsp";
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            request.getRequestDispatcher("homepage.jsp");
+            AccountDAO dao = new AccountDAO();
+            AccountDTO dto = dao.getAccount(userName, passWord);
+            
+            if (dto != null) {
+                request.setAttribute("Error", "Username has been existed");
+            }
+            else {
+                boolean result = dao.insertAccount(userName, passWord, fullName, email, phone);
+                
+                if (result) {
+                    url = "login.html";
+                }
+            }
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
+        }finally{
+            
         }
     }
 
@@ -48,7 +76,11 @@ public class ViewHomePageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -62,7 +94,11 @@ public class ViewHomePageServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
--- src/java/controllers/ViewHomePageServlet.java
+++ src/java/controllers/ViewHomePageServlet.java
@@ -3,26 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller;
+package controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import table.accounts.AccountDAO;
-import table.accounts.AccountDTO;
 
 /**
  *
  * @author long
  */
-public class RegistrationServlet extends HttpServlet {
+public class ViewHomePageServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,35 +26,13 @@ public class RegistrationServlet extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
-     * @throws java.sql.SQLException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String userName = request.getParameter("txtUsername");
-        String passWord = request.getParameter("txtPassword");
-        String email = request.getParameter("txtEmail");
-        String fullName = request.getParameter("txtFullName");
-        String phone = request.getParameter("txtPhone");
-        String url = "registration.jsp";
         try (PrintWriter out = response.getWriter()) {
-            AccountDAO dao = new AccountDAO();
-            AccountDTO dto = dao.getAccount(userName, passWord);
-            
-            if (dto != null) {
-                request.setAttribute("Error", "Username has been existed");
-            }
-            else {
-                boolean result = dao.insertAccount(userName, passWord, fullName, email, phone);
-                
-                if (result) {
-                    url = "login.html";
-                }
-            }
-            RequestDispatcher rd = request.getRequestDispatcher(url);
-            rd.forward(request, response);
-        }finally{
-            
+            /* TODO output your page here. You may use following sample code. */
+            request.getRequestDispatcher("homepage.jsp");
         }
     }
 
@@ -76,11 +48,7 @@ public class RegistrationServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        processRequest(request, response);
     }
 
     /**
@@ -94,11 +62,7 @@ public class RegistrationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        processRequest(request, response);
     }
 
     /**
